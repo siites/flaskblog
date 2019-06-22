b7a2af97af18a7c99e9292c2624a689de44a5727
siites <siites@gmail.com>
2019-06-23 00:55:57 +0900

    register, login 기능 구현

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # flaskblog
+
+###### project 생성
+```
+$ python3 -m venv venv
+
+$ source venv/bin/activate
+
+$ pip install flask
+
+$ pip install flask-wtf
+```
+***
+###### SECRET_KEY 생성
+```
+import secrets
+secrets.token_hex(16)
+```
+
+***
+###### Reference
+https://youtu.be/QnDWIZuWYW0
+
+https://github.com/CoreyMSchafer/code_snippets/tree/master/Python/Flask_Blog/snippets
+
+https://getbootstrap.com/docs/4.3/getting-started/introduction/
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '0737aef98442a5b5028510615e4365e3'
 
 posts = [
     {
@@ -20,13 +22,34 @@ posts = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account create for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have beed logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccess. Please username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
